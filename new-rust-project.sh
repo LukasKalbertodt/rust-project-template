cf636e252b728300f65e87a3b5fe4b18510aa90e
Lukas Kalbertodt <lkalbertodt@uos.de>
2018-03-21 10:07:45 +0100

    Remove .gitignore from template
    Cargo already generates a proper `.gitignore`. Not sure why I
    included it in the first place.

--- new-rust-project.sh
+++ new-rust-project.sh
@@ -29,7 +29,6 @@ fi
 # Copy all the template files
 MY_PATH="`dirname \"$0\"`"
 
-cp $MY_PATH/.gitignore $1/
 cp $MY_PATH/LICENSE-APACHE $1/
 cp $MY_PATH/LICENSE-MIT $1/
 cp $MY_PATH/README.md $1/
@@ -45,9 +44,6 @@ echo ""
 echo "Please don't forget to:"
 echo " - add the project's name in LICENSE-MIT"
 echo " - fix the project's name in README.md"
-if [[ "$2" == "--bin" ]] ; then
-    echo " - Remove 'Cargo.lock' from the .gitignore "
-fi
 echo " - add some useful information to README.md"
 echo " - enable this project on Travis-CI"
 echo ""
